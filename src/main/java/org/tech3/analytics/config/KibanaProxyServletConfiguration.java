260250aadbc257ee48e7e4e69bb4d86514702e77
Aaradhana Sridharan <aaradhana.sridharan@gmail.com>
2018-02-24 18:25:05 +0530

    Application props bean

--- src/main/java/org/tech3/analytics/config/KibanaProxyServletConfiguration.java
+++ src/main/java/org/tech3/analytics/config/KibanaProxyServletConfiguration.java
@@ -2,6 +2,8 @@ package org.tech3.analytics.config;
 
 import org.apache.http.HttpRequest;
 import org.mitre.dsmiley.httpproxy.ProxyServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.EnvironmentAware;
@@ -12,10 +14,16 @@ import org.springframework.core.env.Environment;
 import javax.servlet.http.HttpServletRequest;
 
 @Configuration
-public class KibanaProxyServletConfiguration implements EnvironmentAware {
+public class KibanaProxyServletConfiguration{
 
 
-    private static final String ENV_PROXY_SOLR = "proxy.kibana.";
+    private final Logger log = LoggerFactory.getLogger(KibanaProxyServletConfiguration.class);
+
+    private final ApplicationProperties applicationProperties;
+
+    public KibanaProxyServletConfiguration(ApplicationProperties applicationProperties){
+        this.applicationProperties = applicationProperties;
+    }
 
     @Bean
     public ServletRegistrationBean kibanaServletRegistrationBean(){
@@ -25,20 +33,15 @@ public class KibanaProxyServletConfiguration implements EnvironmentAware {
 
                 String method = servletRequest.getMethod();
                 log("The requested method = " + method);
-                //if(Boolean.valueOf(propertyResolver.getProperty("is_auth_enabled"))) {
-                    //proxyRequest.addHeader("Authorization", "Basic dXNlcjp1c2Vy");
-                //}
+                if(applicationProperties.getProxy().getKibana().isAuthEnabled()) {
+                    proxyRequest.addHeader("Authorization", "Basic dXNlcjp1c2Vy");
+                }
 
                 super.copyRequestHeaders(servletRequest, proxyRequest);
             }
-        }, "/test/");
-        servletRegistrationBean.addInitParameter("targetUri", "http://http-patch-vidhya.1d35.starter-us-east-1.openshiftapps.com/");
-        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, "true");
+        }, applicationProperties.getProxy().getKibana().getServletUrl());
+        servletRegistrationBean.addInitParameter("targetUri", applicationProperties.getProxy().getKibana().getTargetUrl());
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, String.valueOf(applicationProperties.getProxy().getKibana().isLoggingEnabled()));
         return servletRegistrationBean;
-    }
-
-    @Override
-    public void setEnvironment(Environment environment) {
-
     }
 }
